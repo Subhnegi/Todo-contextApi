7f5892558c74e9baf2593274b8431cd9d072d403
Subh <subhamnegi1396@gmail.com>
2024-09-04 06:38:15 +0530

    initial commit

---
+++ todo@ContextApi/README.md
@@ -0,0 +1,3 @@
+#Todo App using Context Api
+created for learning purposes
+data is to be stored and retrieved from local storage
---
+++ todo@ContextApi/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-9xl">working</h1>
+    </>
+  )
+}
+
+export default App
